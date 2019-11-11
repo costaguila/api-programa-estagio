689c99ad89fef330b419e92ef33d06fe088e742e
Guilherme de Almeida <guilherme.almeida@ivoryit.com.br>
2019-11-11 13:45:11 -0300

    Arquivos estáticos

--- apiEstagio/urls.py
+++ apiEstagio/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('apiEstagio.turismo.urls')),
     path('api/turismo',include('apiEstagio.turismo.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
