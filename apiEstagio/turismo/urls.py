c8362c54d58aa264a4ea6cf2442948563c4fd931
Guilherme de Almeida <guilherme.almeida@ivoryit.com.br>
2019-11-11 10:01:38 -0300

    Ajustes nas urls

--- apiEstagio/turismo/urls.py
+++ apiEstagio/turismo/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 from apiEstagio.turismo.views import Turismo
 
 urlpatterns = [
-    path('', Turismo.as_view()),
+    url(r'^$', Turismo.as_view(),name='turismo'),
 
 ]
--- apiEstagio/urls.py
+++ apiEstagio/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('apiEstagio.turismo.urls')),
     path('api/turismo',include('apiEstagio.turismo.urls'))
 ]
